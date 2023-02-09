13b3d165b57a02749c0b0ca759c08ccc76a87ac2
AndresAC14 <andresamo0101@gmail.com>
2023-02-09 21:15:03 +0100

    hello world y cargo

---
+++ hello-world/hello-cargo/target/debug/deps/hello_cargo.d
@@ -0,0 +1,5 @@
+C:\Users\andre\Documents\Andres\Rust-demo\hello-world\hello-cargo\target\debug\deps\hello_cargo.exe: src\main.rs
+
+C:\Users\andre\Documents\Andres\Rust-demo\hello-world\hello-cargo\target\debug\deps\hello_cargo.d: src\main.rs
+
+src\main.rs:
---
+++ hello-world/hello-cargo/target/debug/hello-cargo.d
@@ -0,0 +1,1 @@
+C:\Users\andre\Documents\Andres\Rust-demo\hello-world\hello-cargo\target\debug\hello-cargo.exe: C:\Users\andre\Documents\Andres\Rust-demo\hello-world\hello-cargo\src\main.rs

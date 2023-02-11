043218c9ac4d3cef3e53fef7c05ab611d4b76106
AndresAC14 <andresamo0101@gmail.com>
2023-02-11 20:06:13 +0100

    a

--- hello-world/hello-cargo/src/main.rs
+++ hello-world/hello-cargo/src/main.rs
@@ -1,3 +1,19 @@
 fn main() {
     println!("Hello, world!");
+
+    let numero = 10;
+
+    println!("illo {} ke dise", numero);
+
+    mutables();
+}
+
+fn mutables(){
+    let shadow_num = 5;
+    // para que una variable sea mutable añadir mut entre let y el nombre de la var
+    let shadow_num = shadow_num + 5;
+
+    let shadow_num = shadow_num * 2;
+
+    println!("The number is {}", shadow_num);
 }
